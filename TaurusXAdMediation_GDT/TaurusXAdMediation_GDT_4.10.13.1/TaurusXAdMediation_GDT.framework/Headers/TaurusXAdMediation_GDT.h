35c72da8346a8e5b33a36d862750cd0be579119c
hang.wang <847972062@qq.com>
2019-10-15 11:36:45 +0800

    .

---
+++ TaurusXAdMediation_GDT/TaurusXAdMediation_GDT_4.10.13.1/TaurusXAdMediation_GDT.framework/Headers/TaurusXAdMediation_GDT.h
@@ -0,0 +1,22 @@
+//
+//  TaurusXAdMediation_GDT.h
+//  TaurusXAdMediation_GDT
+//
+//  Created by Matthew on 2019/8/2.
+//  Copyright © 2019年 TXAD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TaurusXAdMediation_GDT.
+FOUNDATION_EXPORT double TaurusXAdMediation_GDTVersionNumber;
+
+//! Project version string for TaurusXAdMediation_GDT.
+FOUNDATION_EXPORT const unsigned char TaurusXAdMediation_GDTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaurusXAdMediation_GDT/PublicHeader.h>
+
+
+#import <TaurusXAdMediation_GDT/TXADGDTExpressFeedListConfig.h>
+#import <TaurusXAdMediation_GDT/TXADGDTExpressNativeConfig.h>
+#import <TaurusXAdMediation_GDT/TXADGDTSplashConfig.h>
