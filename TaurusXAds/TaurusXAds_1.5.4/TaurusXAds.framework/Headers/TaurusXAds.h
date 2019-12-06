3bc562fafb0a513745fbb7a8dd3e5aec83cb83bb
hang.wang <847972062@qq.com>
2019-12-06 15:33:22 +0800

    .

---
+++ TaurusXAds/TaurusXAds_1.5.4/TaurusXAds.framework/Headers/TaurusXAds.h
@@ -0,0 +1,65 @@
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TaurusXAds.
+FOUNDATION_EXPORT double TXADSdkVersionNumber;
+
+//! Project version string for TaurusXAds.
+FOUNDATION_EXPORT const unsigned char TXADSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaurusXAds/PublicHeader.h>
+#import <TaurusXAds/TXAD.h>
+
+#import <TaurusXAds/TXADAdError.h>
+#import <TaurusXAds/TXADAdType.h>
+
+#import <TaurusXAds/TXADAdUnitInfo.h>
+#import <TaurusXAds/TXADAdContentInfo.h>
+
+#import <TaurusXAds/TXADBannerAdSize.h>
+#import <TaurusXAds/TXADBannerView.h>
+#import <TaurusXAds/TXADBannerViewDelegate.h>
+
+#import <TaurusXAds/TXADCLConfig.h>
+
+#import <TaurusXAds/TXADFeed.h>
+#import <TaurusXAds/TXADFeedList.h>
+#import <TaurusXAds/TXADFeedListDelegate.h>
+
+#import <TaurusXAds/TXADILineItem.h>
+
+#import <TaurusXAds/TXADInteractiveArea.h>
+
+#import <TaurusXAds/TXADInterstitialAd.h>
+#import <TaurusXAds/TXADInterstitialAdDelegate.h>
+
+#import <TaurusXAds/TXADMixViewAd.h>
+#import <TaurusXAds/TXADMixViewAdDelegate.h>
+
+#import <TaurusXAds/TXADMixFullScreenAd.h>
+#import <TaurusXAds/TXADMixFullScreenAdDelegate.h>
+
+#import <TaurusXAds/TXADNativeAd.h>
+#import <TaurusXAds/TXADNativeAdLayout.h>
+#import <TaurusXAds/TXADNativeAdDelegate.h>
+#import <TaurusXAds/TXADNativeAdViewRender.h>
+
+#import <TaurusXAds/TXADNetwork.h>
+#import <TaurusXAds/TXADNetworkConfig.h>
+#import <TaurusXAds/TXADNetworkConfigs.h>
+
+#import <TaurusXAds/TXADObjectCache.h>
+#import <TaurusXAds/TXADPluginUtil.h>
+
+#import <TaurusXAds/TXADRewardedVideoAd.h>
+#import <TaurusXAds/TXADRewardedVideoAdDelegate.h>
+#import <TaurusXAds/TXADRewardItem.h>
+
+#import <TaurusXAds/TXADSplashAd.h>
+#import <TaurusXAds/TXADSplashAdDelegate.h>
+
+#import <TaurusXAds/TXADTracker.h>
+#import <TaurusXAds/TXADTrackerInfo.h>
+#import <TaurusXAds/TXADTrackerDelegate.h>
